7ae04e61cbaed685269ca831d3a93d6c32e3b216
sinlov <sinlovgmppt@gmail.com>
2024-11-27 14:12:38 +0800

    test(gitea_token_client): add unit test for ListOrgTeams functionality
    - Implement TestListOrgTeams function to test listing organizations
    - Add necessary imports and setup for the new test
    - Use existing env variable helpers for API key and base URL
    - Include error handling and assertions for test validation

--- gitea_token_client_test/client_test.go
+++ gitea_token_client_test/client_test.go
@@ -1,6 +1,7 @@
 package gitea_token_client_test
 
 import (
+	"code.gitea.io/sdk/gitea"
 	"github.com/sinlov-go/gitea-client-wrapper/gitea_token_client"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -47,6 +48,63 @@ func TestNewClientWithHttpTimeout(t *testing.T) {
 				return
 			}
 			t.Logf("success init as url %s", giteaTokenClient.GetBaseUrl())
+		})
+	}
+}
+
+func TestListOrgTeams(t *testing.T) {
+	if envMustArgsCheck(t) {
+		return
+	}
+	// mock Name
+	type args struct {
+		url           string
+		accessToken   string
+		timeoutSecond uint
+		insecure      bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "sample",
+			args: args{
+				url:           valEnvGiteaBaseUrl,
+				accessToken:   valEnvGiteaApiKey,
+				timeoutSecond: 30,
+				insecure:      valEnvGiteaInsecure,
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+
+			// do ListOrgTeams
+			giteaTokenClient := gitea_token_client.GiteaTokenClient{}
+
+			// do NewClientWithHttpTimeout
+			gotErr := giteaTokenClient.NewClientWithHttpTimeout(tc.args.url, tc.args.accessToken, tc.args.timeoutSecond, tc.args.insecure)
+
+			// verify ListOrgTeams
+			assert.Equal(t, tc.wantErr, gotErr != nil)
+			if tc.wantErr {
+				return
+			}
+			t.Logf("success init as url %s", giteaTokenClient.GetBaseUrl())
+
+			giteaClient := giteaTokenClient.GiteaClient()
+			opt := gitea.ListOrgsOptions{
+				ListOptions: gitea.ListOptions{
+					Page:     1,
+					PageSize: 10,
+				},
+			}
+			orgs, response, gotErr := giteaClient.ListMyOrgs(opt)
+			assert.Equal(t, tc.wantErr, gotErr != nil)
+			t.Logf("response StatusCode: %v", response.StatusCode)
+			t.Logf("orgs: %v", orgs)
 		})
 	}
 }
